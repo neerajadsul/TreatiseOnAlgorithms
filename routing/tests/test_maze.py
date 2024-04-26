0a6ef95812ee5dbc252d295d7b14f2eda5499426
neerajadsul <neeraj.adsul@gmail.com>
2024-04-26 11:15:53 +0100

    Tests for planned functionality for maze load, save and next moves calculation.

---
+++ routing/tests/test_maze.py
@@ -0,0 +1,20 @@
+def test_load_maze():
+    """Tests loading a 2-D maze from a text file."""
+    pass
+
+
+def test_save_maze_as_text():
+    """Test saving 2-D maze in a text file.
+    """
+    pass
+
+
+def test_save_maze_as_binary():
+    """Test saving 2-D maze in a binary file.
+    """
+    pass
+
+def test_next_moves_given_current_position():
+    """Test getting next possible moves in a maze given current position.
+    """
+    pass
