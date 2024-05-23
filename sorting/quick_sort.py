263d03efd183024d5b2cb85b79a7f59b40740599
neerajadsul <neeraj.adsul@gmail.com>
2024-05-23 15:40:11 +0100

    Deque based implementation for quick -sort algorithm.

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -6,6 +6,9 @@
 Then recusively sort each bucket.
 At the end, combine the buckets together as left + middle + right.
 """
+from queue import deque
+from typing import Deque
+
 
 def quick_sort_lists(S):
     n = len(S)
@@ -34,8 +37,45 @@ def quick_sort_lists(S):
     while len(right) > 0:
         S.append(right.pop(0))
     
+
+def quick_sort_deque(S: Deque):
+    if not isinstance(S, Deque):
+        raise ValueError('Expected sequence of Deque type')
+    n = len(S)
+    if n < 2:
+        return
+        
+    pivot = S.pop()
+    
+    lower, middle, higher = deque(), deque(), deque()
+    middle.append(pivot)
+    
+    while len(S) > 0:
+        m = S.pop()
+        if m < pivot:
+            lower.append(m)
+        elif m > pivot:
+            higher.append(m)
+        else:
+            middle.append(m)
+    
+    quick_sort_deque(lower)
+    quick_sort_deque(higher)
+    
+    while len(lower) > 0:
+        S.append(lower.popleft())
+    while len(middle) > 0:
+        S.append(middle.popleft())
+    while len(higher) > 0:
+        S.append(higher.popleft())
+    
+   
     
 if __name__ == "__main__":
     S = [4, -2, 3, 0, -6]
     quick_sort_lists(S)
+    print(S)
+
+    S = deque([4, -2, 3, 0, -6])
+    quick_sort_deque(S)
     print(S)
